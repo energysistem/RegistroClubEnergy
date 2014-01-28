671316ace766eef5fd35845045cfbb9faa0da555
unknown <AGE@cpu117.avalon.lan>
2014-01-28 10:24:10 +0100

    First Commit

---
+++ app/src/main/java/es/energy/clubenergy/MainActivity.java
@@ -0,0 +1,52 @@
+package es.energy.clubenergy;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import java.io.IOException;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        KillStatusBar();
+        WebView webView =(WebView)this.findViewById(R.id.webView);
+        // Enable JavaScript
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://energysistem/thader/registro");
+
+
+    }
+    private void KillStatusBar()
+    {
+        Process proc = null;
+
+        String ProcID = "79"; //HONEYCOMB AND OLDER
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH){
+            ProcID = "42"; //ICS AND NEWER
+        }
+
+        try {
+            proc = Runtime.getRuntime().exec(new String[]{"su", "-c", "service call activity " + ProcID + " s16 com.android.systemui"});
+        } catch (IOException e) {
+            Log.d("TAG", "Failed to kill task bar (1).");
+            e.printStackTrace();
+        }
+        try {
+            proc.waitFor();
+        } catch (InterruptedException e) {
+            Log.d("TAG","Failed to kill task bar (2).");
+            e.printStackTrace();
+        }
+    }
+
+
+}
